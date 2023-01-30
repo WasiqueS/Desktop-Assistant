998763d332e4763fade65add147e2ed4ca53752a
WasiqueS <59787747+WasiqueS@users.noreply.github.com>
2023-01-30 16:09:48 +0530

    Add files via upload

---
+++ bob.py
@@ -0,0 +1,136 @@
+import speech_recognition as sr
+import pyttsx3
+import os
+import datetime
+from tkinter import *              #For the graphics
+import pyaudio
+import wikipedia
+import webbrowser
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Bob Sir. Please tell me how may I help you") 
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        said = ""
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except sr.UnknownValueError:
+            speak("Sorry, I did not get that.")
+        except sr.RequestError:
+            speak("Sorry, the service is not available")    
+    return said.lower() 
+
+def tellDay():
+      
+    # This function is for telling the
+    # day of the week
+    day = datetime.datetime.today().weekday() + 1
+      
+    #this line tells us about the number 
+    # that will help us in telling the day
+    Day_dict = {1: 'Monday', 2: 'Tuesday', 
+                3: 'Wednesday', 4: 'Thursday', 
+                5: 'Friday', 6: 'Saturday',
+                7: 'Sunday'}
+      
+    if day in Day_dict.keys():
+        day_of_the_week = Day_dict[day]
+        print(day_of_the_week)
+        speak("The day is " + day_of_the_week)
+
+
+if __name__ == "__main__":
+    wishMe()
+
+while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+        
+        elif "open cpp" in query:
+            codePath = "C:\\Program Files (x86)\\Dev-Cpp\\devcpp.exe"
+            os.startfile(codePath)
+        
+        elif "open zoom" in query:
+            codePath = "C:\\Users\\DELL\\AppData\\Roaming\\Zoom\\bin\\Zoom.exe"
+            os.startfile(codePath)
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\DELL\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
+            
+        if "open geeksforgeeks" in query:
+            speak("Opening GeeksforGeeks ")
+            webbrowser.open("www.geeksforgeeks.com")
+
+        elif "which day it is" in query:
+            tellDay()
+        
+        elif "exit" in query:
+         speak("Exiting. Have a Good Day")
+         exit()
+
+         
